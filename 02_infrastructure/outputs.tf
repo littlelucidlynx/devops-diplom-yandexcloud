c2dd81a5551800df8f22867b5c7ee450ff5c8d24
littlelucidlynx <dmitry.man5on@gmail.com>
2025-05-05 18:56:50 +0300

    update outputs

--- 02_infrastructure/outputs.tf
+++ 02_infrastructure/outputs.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = try(yandex_kubernetes_cluster.k8s-regional.id, null)
+  value = try(yandex_kubernetes_cluster.regional_cluster.id, null)
 }
 
 output "cluster_name" {
-  value = try(yandex_kubernetes_cluster.k8s-regional.name, null)
+  value = try(yandex_kubernetes_cluster.regional_cluster.name, null)
 }
 
 output "external_v4_address" {
-  value = yandex_kubernetes_cluster.k8s-regional.master[0].external_v4_address
+  value = yandex_kubernetes_cluster.regional_cluster.master[0].external_v4_address
 }
 
 output "external_cluster_cmd" {
-  value = "yc managed-kubernetes cluster get-credentials --id ${yandex_kubernetes_cluster.k8s-regional.id} --external"
+  value = "yc managed-kubernetes cluster get-credentials --id ${yandex_kubernetes_cluster.regional_cluster.id} --external"
 }
